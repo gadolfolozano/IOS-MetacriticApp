390844a65665a07e86e9465e0cbc9bc48356743f
Gustavo Adolfo Lozano Mendez <glozano@matterport.com>
2023-01-12 23:23:44 -0300

    Initial Commit

---
+++ Shared/Metacritic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metacritic_AppApp.swift
+//  Shared
+//
+//  Created by Gustavo Lozano on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Metacritic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
